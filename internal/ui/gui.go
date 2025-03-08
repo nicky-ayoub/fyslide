0de19572177eaf6856fb8e7b7a32ec7ed91ecc1e
Nicky Ayoub <2052886+nicky-ayoub@users.noreply.github.com>
2025-03-08 10:01:46 -0700

    Refactored gui functions in to app if they did not actually build the GUI

--- internal/ui/gui.go
+++ internal/ui/gui.go
@@ -1,12 +1,8 @@
 package ui
 
 import (
-	"fmt"
-	"image"
 	"log"
-	"os"
 	"runtime"
-	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -17,88 +13,6 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-func (a *App) DisplayImage() error {
-	// decode and update the image + get image path
-	//fmt.Printf("Displaying %s\n", a.images[a.index].Path)
-	var err error
-	file, err := os.Open(a.images[a.index].Path)
-	if err != nil {
-		return fmt.Errorf("unable to open image '%s' %v", a.images[a.index].Path, err)
-	}
-	image, name, err := image.Decode(file)
-	if err != nil {
-		fmt.Printf("Unable image.Decode(%q) of format %q\n", file.Name(), name)
-		return fmt.Errorf("unable to decode image %v", err)
-	}
-	a.img.OriginalImage = image
-	a.img.Path = file.Name()
-	a.image.Image = a.img.OriginalImage
-	a.image.Refresh()
-
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return fmt.Errorf("unable to get file info %v", err)
-	}
-
-	a.imgSize.SetText(fmt.Sprintf("Size: %d bytes", fileInfo.Size()))
-	a.imgLastMod.SetText(fmt.Sprintf("Last modified: \n%s", fileInfo.ModTime().Format("02-01-2006")))
-	w := fmt.Sprintf("Width:   %dpx", a.img.OriginalImage.Bounds().Max.X)
-	h := fmt.Sprintf("Height: %dpx", a.img.OriginalImage.Bounds().Max.Y)
-	c := fmt.Sprintf("Count: %d", a.ImageCount())
-	a.widthLabel.SetText(w)
-	a.heightLabel.SetText(h)
-	a.countLabel.SetText(c)
-
-	a.MainWin.SetTitle(fmt.Sprintf("FySlide - %v", (strings.Split(a.img.Path, "/")[len(strings.Split(a.img.Path, "/"))-1])))
-	a.statusLabel.SetText(fmt.Sprintf("Image %s, %d of %d", a.img.Path, a.index+1, len(a.images)))
-
-	a.leftArrow.Enable()
-	a.rightArrow.Enable()
-	a.first.Enable()
-	a.last.Enable()
-	return nil
-}
-
-func (a *App) firstImage() {
-	a.index = 0
-	a.DisplayImage()
-	a.direction = 1
-
-}
-
-func (a *App) lastImage() {
-	a.index = len(a.images) - 1
-	a.DisplayImage()
-	a.direction = -1
-
-}
-
-func (a *App) nextImage() {
-	a.index += a.direction
-	if a.index < 0 {
-		a.direction = 1
-		a.index = 0
-	} else if a.index >= len(a.images) {
-		a.direction = -1
-		a.index = len(a.images) - 1
-	}
-	a.DisplayImage()
-}
-
-func (a *App) tagFile() {
-	dialog.ShowCustom("TAGGER", "Ok", container.NewVBox(
-		widget.NewLabel("Add image tag."),
-		widget.NewHyperlink("Help and more information on Github", parseURL("https://github.com/nicky-ayoub/fyslide")),
-		widget.NewLabel("v1.2 | License: MIT"),
-	), a.MainWin)
-}
-func (a *App) deleteFileCheck() {
-	dialog.ShowConfirm("Delete file!", "Are you sure?\n This action can't be undone.", func(b bool) {
-		if b {
-			a.deleteFile()
-		}
-	}, a.MainWin)
-}
 func (a *App) buildSatusBar() *fyne.Container {
 	a.first = widget.NewButtonWithIcon("", theme.MediaFastRewindIcon(), a.firstImage)
 	a.leftArrow = widget.NewButtonWithIcon("", theme.NavigateBackIcon(), func() { a.direction = -1; a.nextImage() })
@@ -147,18 +61,6 @@ func (a *App) buildInformationTab() *container.TabItem {
 			a.imgLastMod,
 		),
 	))
-}
-
-func (a *App) togglePlay() {
-	if a.paused {
-		a.paused = false
-		a.pauseBtn.SetIcon(theme.MediaPauseIcon())
-		a.toolbar.Items[2].(*widget.ToolbarAction).SetIcon(theme.MediaPauseIcon())
-	} else {
-		a.paused = true
-		a.pauseBtn.SetIcon(theme.MediaPlayIcon())
-		a.toolbar.Items[2].(*widget.ToolbarAction).SetIcon(theme.MediaPlayIcon())
-	}
 }
 
 func (a *App) buildToolbar() *widget.Toolbar {
@@ -248,26 +150,4 @@ func (a *App) buildMainUI() fyne.CanvasObject {
 		nil,       // right
 		a.split,
 	)
-}
-
-func (a *App) deleteFile() {
-	if err := os.Remove(a.img.Path); err != nil {
-		dialog.NewError(err, a.MainWin)
-		return
-	}
-	if a.index == len(a.images)-1 {
-		a.direction = -1
-		a.nextImage()
-	} else if len(a.images) == 1 {
-		a.direction = 1
-		a.image.Image = nil
-		a.img.EditedImage = nil
-		a.img.OriginalImage = nil
-		a.rightArrow.Disable()
-		a.leftArrow.Disable()
-		a.deleteBtn.Disable()
-		a.image.Refresh()
-	} else {
-		a.nextImage()
-	}
 }
